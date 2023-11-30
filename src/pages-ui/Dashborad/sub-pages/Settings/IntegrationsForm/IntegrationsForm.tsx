290682f272b117a262bbecdd7fcd3213da544333
danesto <dane.sto@gmail.com>
2023-11-30 22:46:44 +0100

    add slack button logic

--- src/pages-ui/Dashborad/sub-pages/Settings/IntegrationsForm/IntegrationsForm.tsx
+++ src/pages-ui/Dashborad/sub-pages/Settings/IntegrationsForm/IntegrationsForm.tsx
@@ -1,29 +1,51 @@
-import { Image, Link } from '@/components/Chakra';
+import { Alert, Button, Image, Link, Text } from '@/components/Chakra';
 import NextLink from 'next/link';
 import SettingsSection from '../SettingsSection';
 import styles from '../settings.module.scss';
-
-const SLACK_CLIENT_ID = process.env.SLACK_CLIENT_ID;
+import { useSearchParams } from 'next/navigation';
 
 function IntegrationsForm() {
+  const SLACK_CLIENT_ID = process.env.NEXT_PUBLIC_SLACK_CLIENT_ID;
+  const searchParams = useSearchParams();
+  const slackAuthCode = searchParams.get('code');
+
   return (
     <SettingsSection
       title="Slack notifications"
       subtitle="You'll receieve notifications directly on your Slack selected channel if parking spot gets released"
     >
-      <Link
-        as={NextLink}
-        href={`https://slack.com/oauth/v2/authorize?client_id=${SLACK_CLIENT_ID}&scope=incoming-webhook&user_scope=`}
-        className={styles.slackLink}
-      >
-        <Image
-          alt="Add to Slack"
-          height="40px"
-          width="139px"
-          src="https://platform.slack-edge.com/img/add_to_slack.png"
-          srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-        />
-      </Link>
+      {slackAuthCode ? (
+        <Alert
+          colorScheme="yellow"
+          variant="left-accent"
+          flexDir="column"
+          gap="20px"
+          alignItems="flex-start"
+        >
+          <Text>
+            You have authorized your Slack organization,{' '}
+            <span>
+              <strong>please click button below</strong>{' '}
+            </span>
+            to finish things up and install the app.
+          </Text>
+          <Button colorScheme="blue">Finish setting up</Button>
+        </Alert>
+      ) : (
+        <Link
+          as={NextLink}
+          href={`https://slack.com/oauth/v2/authorize?client_id=${SLACK_CLIENT_ID}&scope=incoming-webhook&user_scope=`}
+          className={styles.slackLink}
+        >
+          <Image
+            alt="Add to Slack"
+            height="40px"
+            width="139px"
+            src="https://platform.slack-edge.com/img/add_to_slack.png"
+            srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+          />
+        </Link>
+      )}
     </SettingsSection>
   );
 }
---
+++ src/pages-ui/Dashborad/sub-pages/Settings/IntegrationsForm/actions.ts
@@ -0,0 +1,5 @@
+'use server';
+
+const integrateSlackWebhook = () => {
+  //TODO: fire auth event to get the Slack webhook and store it in the database
+};
